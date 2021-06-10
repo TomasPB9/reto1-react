71b731f181c40f3eb1a801c72c3c3b61e827b448
Tomas Bolio <tomas_9724@hotmail.com>
2021-06-09 21:22:04 -0500

    adding stateful components

--- src/components/FilaSuperior.js
+++ src/components/FilaSuperior.js
@@ -4,11 +4,11 @@ import Manteles from './Manteles';
 const FilaSuperior = () => {
   return (
     <div className="filaSuperior">
-      <Manteles />
-      <Manteles />
-      <Manteles />
+      <Manteles color="green" height="140px" />
+      <Manteles height="170px"/>
+      <Manteles color="purple" height="110px" />
     </div>
-  )
+  );
 }
 
 export default FilaSuperior;
--- src/components/Manteles.js
+++ src/components/Manteles.js
@@ -1,15 +1,41 @@
-import '../css/Manteles.css';
-import Cubiertos from './Cubiertos';
-import Platos from './Platos';
+import React from 'react';
+import "../css/Manteles.css";
+import Cubiertos from "./Cubiertos";
+import Platos from "./Platos";
+//good practices with props
+import PropTypes from 'prop-types';
 
-const Manteles = () => {
-  return (
-    <div className="manteles">
-      <Cubiertos />
-      <Platos />
-      <Cubiertos />
-    </div>
-  )
+class Manteles extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      color: 'darksalmon',
+      height: '120px',
+    }
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: this.props.color || this.state.color,
+          height: this.props.height || this.state.height,
+        }}
+        className="manteles"
+      >
+        <Cubiertos />
+        <Platos />
+        <Cubiertos />
+      </div>
+    );
+  }
+}
+
+//good practice
+Manteles.propTypes = {
+  color: PropTypes.string,
+  height: PropTypes.string,
 }
 
 export default Manteles;
